7f56d18bec416cd96574fc707406666839ec0e2c
Lewis Moore <lewis.moore2707@gmail.com>
2018-07-05 13:55:39 +0100

    Add variable declaration before test cases. Add second test when given any other number

--- src/test/java/FizzBuzz/AppTest.java
+++ src/test/java/FizzBuzz/AppTest.java
@@ -3,13 +3,18 @@ package FizzBuzz;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class AppTest 
-{
+public class AppTest {
+
+    App fizzbuzz = new App();
+
     @Test
     public void ThreeReturnsFizz() {
-        App fizzbuzz = new App();
-
         Assert.assertEquals("Fizz", fizzbuzz.convert(3));
 
+    }
+
+    @Test
+    public void ReturnsInput() {
+        Assert.assertEquals("1", fizzbuzz.convert(1));
     }
 }
